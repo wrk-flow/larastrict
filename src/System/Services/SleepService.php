6bd605664dc1cad0bdb4f238b7a1991df917a625
Martin Kluska <martin@kluska.cz>
2022-04-19 17:59:02 +0200

    Add sleep service for mocking sleep commands

---
+++ src/System/Services/SleepService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaraStrict\System\Services;
+
+class SleepService
+{
+    public function sleep(int $milliSeconds): void
+    {
+        usleep($milliSeconds * 1000);
+    }
+
+    public function sleepRandom(int $fromMilliSeconds, int $toMilliSeconds): void
+    {
+        $this->sleep(random_int($fromMilliSeconds, $toMilliSeconds));
+    }
+}
