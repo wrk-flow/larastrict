b43a938171b0cb305114029d99051a90ded38271
Martin Kluska <martin@kluska.cz>
2022-04-28 16:14:14 +0200

    Database: Improve nexted relation scope with has support (instead of relation loading)

--- src/Database/Scopes/AbstractNestedRelationScope.php
+++ src/Database/Scopes/AbstractNestedRelationScope.php
@@ -6,31 +6,50 @@ namespace LaraStrict\Database\Scopes;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\RelationNotFoundException;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 abstract class AbstractNestedRelationScope extends AbstractScope
 {
-    public function __construct(private readonly array $relationScopes = [])
-    {
+    public function __construct(
+        private readonly array $relationScopes = [],
+        private readonly bool $useHas = false,
+    ) {
     }
 
     public function apply(Builder $builder, Model $model): void
     {
-        $relations = [];
+        $closure = function (QueryBuilder|Relation|Builder $query) use ($model): void {
+            if ($query instanceof Relation) {
+                $relationModel = $query->getRelated();
+                $baseQuery = $query->getQuery();
+            } else {
+                $relationName = $this->getRelationName();
 
-        foreach ($this->getRelationNames() as $relationName) {
-            $relations[$relationName] = function (Relation $relationQuery): void {
-                $model = $relationQuery->getRelated();
-                $baseQuery = $relationQuery->getQuery();
-
-                foreach ($this->relationScopes as $relationScope) {
-                    $relationScope->apply($baseQuery, $model);
+                if (method_exists($model, $relationName) === false) {
+                    throw RelationNotFoundException::make($model, $relationName);
                 }
-            };
-        }
 
-        $builder->with($relations);
+                /** @var Relation<Model> $relation */
+                $relation = $model->{$relationName}();
+                $relationModel = $relation->getRelated();
+                $baseQuery = $query;
+            }
+
+            foreach ($this->relationScopes as $relationScope) {
+                $relationScope->apply($baseQuery, $relationModel);
+            }
+        };
+
+        if ($this->useHas) {
+            $builder->whereHas($this->getRelationName(), $closure);
+        } else {
+            $builder->with([
+                $this->getRelationName() => $closure,
+            ]);
+        }
     }
 
-    abstract protected function getRelationNames(): array;
+    abstract protected function getRelationName(): string;
 }
