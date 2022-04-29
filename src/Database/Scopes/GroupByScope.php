6072ff991483cc3ad55b596d257b3cb5bb12e845
Martin Kluska <martin@kluska.cz>
2022-04-29 19:02:51 +0200

    Database: Add GroupByScope

---
+++ src/Database/Scopes/GroupByScope.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaraStrict\Database\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class GroupByScope extends AbstractScope
+{
+    /**
+     * @param array<string> $groups
+     */
+    public function __construct(private readonly array $groups)
+    {
+    }
+
+    public function apply(Builder $builder, Model $model): void
+    {
+        $builder->groupBy($this->groups);
+    }
+}
