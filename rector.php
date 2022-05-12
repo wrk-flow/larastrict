0a695506753b8cd088279d0e613ad8d9ca22a0b9
Martin Kluska <martin@kluska.cz>
2022-05-12 15:34:13 +0200

    Upgrade rector

--- rector.php
+++ rector.php
@@ -7,32 +7,29 @@ use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\Strict\Rector\AbstractFalsyScalarRuleFixerRector;
 use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
 use Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
-
 return static function (RectorConfig $config): void {
 
-    // get parameters
-    $parameters = $config->parameters();
-    $parameters->set(Option::PATHS, [
+    $config->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-
-    $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_81);
+    $config->phpVersion(PhpVersion::PHP_81);
 
     // Define what rule sets will be applied
     $config->import(LevelSetList::UP_TO_PHP_81);
-
     $config->import(SetList::CODE_QUALITY);
     $config->import(SetList::CODING_STYLE);
+    $config->importNames();
 
-    $services = $config->services();
-    $services->set(AddVoidReturnTypeWhereNoReturnRector::class);
-    $services->set(BooleanInBooleanNotRuleFixerRector::class)->configure([
-        BooleanInTernaryOperatorRuleFixerRector::TREAT_AS_NON_EMPTY => false,
-    ]);
-
+    $config->ruleWithConfiguration(
+        BooleanInBooleanNotRuleFixerRector::class,
+        [
+            AbstractFalsyScalarRuleFixerRector::TREAT_AS_NON_EMPTY => false,
+        ]
+    );
 };
