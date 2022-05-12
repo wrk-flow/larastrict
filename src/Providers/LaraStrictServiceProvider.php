1c54dbe6fb3f800335e676202abac59dfa340a52
Martin Kluska <martin@kluska.cz>
2022-05-12 15:34:05 +0200

    Core: Force factories in same folder as model is placed (Models\MyModelFactory)

--- src/Providers/LaraStrictServiceProvider.php
+++ src/Providers/LaraStrictServiceProvider.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace LaraStrict\Providers;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 use LaraStrict\Cache\CacheServiceProvider;
 use LaraStrict\Console\Contracts\ScheduleServiceContract;
 use LaraStrict\Console\Services\ScheduleServiceService;
@@ -22,5 +25,37 @@ class LaraStrictServiceProvider extends ServiceProvider
         // Add ability to "switch" the implementation.
         $this->app->singleton(ScheduleServiceContract::class, ScheduleServiceContract::class);
         $this->app->alias(ScheduleServiceService::class, ScheduleServiceContract::class);
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // TODO config
+        $this->setupFactoryResolving();
+
+        // TODO config
+        // Model::preventLazyLoading(true);
+    }
+
+    /**
+     * We want to place factories in same folder as the model.
+     */
+    protected function setupFactoryResolving(): void
+    {
+        Factory::guessFactoryNamesUsing(function (string $class) {
+            /** @var class-string<Factory<Model>> $factoryClass */
+            $factoryClass = $class . 'Factory';
+
+            return $factoryClass;
+        });
+
+        Factory::guessModelNamesUsing(function (Factory $factory) {
+            /** @var class-string<Model> $class */
+            $class = Str::replaceLast('Factory', '', $factory::class);
+
+            return $class;
+        });
     }
 }
