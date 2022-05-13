7eb6f3333e177aa03fd2cac826ecb44eb0bf5672
Martin Kluska <martin@kluska.cz>
2022-05-13 19:25:46 +0200

    Add a way to access translations in type strict way

---
+++ src/Translations/AbstractTranslations.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaraStrict\Translations;
+
+use Countable;
+use Illuminate\Contracts\Translation\Translator;
+
+/**
+ * - Makes translations testable
+ * - Forces us making type strict access of translations
+ * - Prevent reusing `file` name string (automatically wraps the key)
+ */
+abstract class AbstractTranslations
+{
+    public function __construct(
+        private readonly Translator $translator
+    ) {
+    }
+
+    /**
+     * Returns localization path (file name)
+     */
+    abstract public function getLocalizationKey(): string;
+
+    /**
+     * @return string|array<string>
+     */
+    protected function get(string $key, array $replace = [], ?string $locale = null): string|array
+    {
+        return $this->translator->get($this->getKey($key), $replace, $locale);
+    }
+
+    protected function choice(
+        string $key,
+        int|array|Countable $number,
+        array $replace = [],
+        ?string $locale = null
+    ): string {
+        return $this->translator->choice($this->getKey($key), $number, $replace, $locale);
+    }
+
+    private function getKey(string $key): string
+    {
+        return $this->getLocalizationKey() . '.' . $key;
+    }
+}
