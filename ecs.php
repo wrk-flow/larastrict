ec17b37fe4ce3a13d27ea22f4b18e1c62de0158e
Martin Kluska <martin@kluska.cz>
2022-09-12 16:17:51 +0200

    Update ecs config
    Signed-off-by: Martin Kluska <martin@kluska.cz>

--- ecs.php
+++ ecs.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use PhpCsFixer\Fixer\ControlStructure\YodaStyleFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $containerConfigurator): void {
@@ -13,14 +12,9 @@ return static function (ECSConfig $containerConfigurator): void {
     $containerConfigurator->import(SetList::COMMON);
     $containerConfigurator->import(SetList::CLEAN_CODE);
 
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PARALLEL, true);
-
-    $parameters->set(
-        Option::PATHS,
-        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php', __DIR__ . '/rector.php']
+    $containerConfigurator->parallel();
+    $containerConfigurator->paths(
+        [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php',  __DIR__ . '/rector.php']
     );
-
-    $parameters->set(Option::SKIP, [YodaStyleFixer::class]);
+    $containerConfigurator->skip([YodaStyleFixer::class]);
 };
