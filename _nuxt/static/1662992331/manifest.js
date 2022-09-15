55804d4b4bdfea234615b707c0cb692b2328b8b4
pionl <martin@kluska.cz>
2022-09-15 15:12:06 +0000

    Deploying to gh-pages from @ wrk-flow/larastrict@1bc4fc83309b743fc11c790ce90588f94eb9552a 🚀

--- _nuxt/static/1662992331/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fservice-provider","\u002Fconsole","\u002Ftesting","\u002Flogging","\u002Fdatabase\u002Fscopes","\u002Fdatabase\u002Fqueries","\u002Fapi\u002Fhealth","\u002Fcore\u002Fcore","\u002Fdatabase\u002Fmodels","\u002Fdatabase\u002Fevents","\u002Fdatabase\u002Fmigrations","\u002Fdocker"]})
