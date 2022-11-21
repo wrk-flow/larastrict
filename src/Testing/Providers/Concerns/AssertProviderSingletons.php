47b13422d8e7d29ec1f935fc2cdd5836cedbaf16
Martin Kluska <martin@kluska.cz>
2022-11-21 11:11:16 +0100

    feat(Testing): Add AssertProviderSingletons for testing singletons

---
+++ src/Testing/Providers/Concerns/AssertProviderSingletons.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaraStrict\Testing\Providers\Concerns;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+use PHPUnit\Framework\Assert;
+
+trait AssertProviderSingletons
+{
+    /**
+     * @param array<class-string|string>   $expectedMap
+     * @param class-string<ServiceProvider>|null $registerServiceProvider
+     */
+    public function assertSingletons(
+        Application $application,
+        array $expectedMap,
+        ?string $registerServiceProvider = null
+    ): void {
+        if ($registerServiceProvider !== null) {
+            $application->register($registerServiceProvider);
+        }
+
+        foreach ($expectedMap as $binding) {
+            $object = $application->make($binding);
+            $singleton = $application->make($binding);
+
+            Assert::assertSame($singleton, $object);
+        }
+    }
+}
---
+++ tests/Feature/Testing/Providers/Concerns/AssertProviderSingletonsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\LaraStrict\Feature\Testing\Providers\Concerns;
+
+use LaraStrict\Testing\Providers\Concerns\AssertProviderSingletons;
+use Tests\LaraStrict\Feature\TestCase;
+use Tests\LaraStrict\Feature\Testing\Commands\MakeExpectationCommand\TestAction;
+
+class AssertProviderSingletonsTest extends TestCase
+{
+    use AssertProviderSingletons;
+
+    final public const KeyBinding = 'test-action';
+
+    public function test(): void
+    {
+        $this->app()
+            ->singleton(self::KeyBinding, TestAction::class);
+
+        $this->assertSingletons($this->app(), [self::KeyBinding]);
+    }
+}
