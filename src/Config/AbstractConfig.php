449ba5c158618bc869ab965d58effe3ca1bced7d
Martin Kluska <martin@kluska.cz>
2023-04-27 16:01:35 +0200

    feat(Config): Add ability to set value

--- src/Config/AbstractConfig.php
+++ src/Config/AbstractConfig.php
@@ -19,21 +19,34 @@ abstract class AbstractConfig
     abstract protected function getConfigFileName(): string;
 
     /**
-     * @param string[]|string $keyOrPath      Provide path that will be joined to dot notation
+     * @param string[]|string $keyOrPath Provide path that will be joined to dot notation
      * @param bool            $emptyToDefault Convert empty value from repository to $default value.
      */
     protected function get(string|array $keyOrPath, mixed $default = '', bool $emptyToDefault = false): mixed
     {
-        if (is_array($keyOrPath)) {
-            $keyOrPath = implode('.', $keyOrPath);
-        }
-
-        $result = $this->config->get($this->configFileName . '.' . $keyOrPath, $default);
+        $result = $this->config->get($this->getPath($keyOrPath), $default);
 
         if ($emptyToDefault && ((is_string($result) === false && is_bool($result)) || empty($result))) {
             return $default;
         }
 
         return $result;
+    }
+
+    /**
+     * @param string[]|string $keyOrPath Provide path that will be joined to dot notation
+     */
+    protected function set(string|array $keyOrPath, mixed $value = null): void
+    {
+        $this->config->set($this->getPath($keyOrPath), $value);
+    }
+
+    protected function getPath(array|string $keyOrPath): string
+    {
+        if (is_array($keyOrPath)) {
+            $keyOrPath = implode('.', $keyOrPath);
+        }
+
+        return $this->configFileName . '.' . $keyOrPath;
     }
 }
