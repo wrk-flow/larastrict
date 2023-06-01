9a89af5ec40a8d63030fe3c69d438cd3f8a82668
Milan Matějček <milan.matejcek@gmail.com>
2023-06-01 11:28:53 +0200

    fix(Rules): Improve NumberRule range of supported int/float values

--- src/Validation/Rules/NumberRule.php
+++ src/Validation/Rules/NumberRule.php
@@ -10,24 +10,32 @@ class NumberRule implements Rule
 {
     public function passes($attribute, $value): bool
     {
-        if (is_string($value)) {
-            $value = str_replace(',', '.', $value);
-        }
-
-        if ((is_float($value)) || (is_numeric($value) && str_contains((string) $value, '.'))) {
-            $floatVal = (float) $value;
-            $stringVal = (string) $floatVal;
-            return is_finite($floatVal) && $stringVal === (string) $value;
-        } elseif (is_numeric($value)) {
+        if (self::isNumericInt($value)) {
             $intVal = (int) $value;
             return $intVal !== PHP_INT_MAX && $intVal !== PHP_INT_MIN;
         }
 
-        return false;
+        $value = strtr((string) $value, [
+            ',' => '.',
+        ]);
+
+        if (is_numeric($value) === false) {
+            return false;
+        }
+
+        return str_contains((string) (float) $value, 'E+') === false;
     }
 
     public function message(): string
     {
         return 'Given :attribute is not a valid number or it exceeds int/float limits.';
+    }
+
+    /**
+     * @return ($value is non-empty-string ? bool : ($value is int ? true : false))
+     */
+    private static function isNumericInt(mixed $value): bool
+    {
+        return is_int($value) || (is_string($value) && preg_match('#^[+-]?\d+$#D', $value));
     }
 }
