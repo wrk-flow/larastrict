cf4a5883cdbac77846e7309ffd167d3ea6360228
Milan Matějček <milan.matejcek@gmail.com>
2024-07-30 07:07:18 +0200

    feat(SqlTestEnable): add fake connector for sql lite

---
+++ src/Tests/Traits/SqlTestEnable.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaraStrict\Tests\Traits;
+
+use Illuminate\Contracts\Database\Query\Builder;
+use Illuminate\Database\ConnectionResolver;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\SQLiteConnection;
+use PDO;
+use PHPUnit\Framework\Assert;
+
+trait SqlTestEnable
+{
+    /**
+     * @beforeClass
+     */
+    final public static function beforeClassSqlTestEnable(): void
+    {
+        $resolver = new ConnectionResolver([
+            'default' => new SQLiteConnection(new PDO('sqlite::memory:')),
+        ]);
+        $resolver->setDefaultConnection('default');
+        Model::setConnectionResolver($resolver);
+    }
+
+    final protected static function assertQuerySql(
+        string $expectedSql,
+        array $expectedBindings,
+        Builder $query,
+    ): void {
+        Assert::assertSame(trim($expectedSql), $query->toSql());
+        Assert::assertSame($expectedBindings, $query->getBindings());
+    }
+}
