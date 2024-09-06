f85f6c501ca77eb44277ac507aa1d054be623577
Milan Matějček <milan.matejcek@gmail.com>
2024-09-06 07:46:42 +0200

    feat(SqlTestEnable): add method assertSql()

--- src/Tests/Traits/SqlTestEnable.php
+++ src/Tests/Traits/SqlTestEnable.php
@@ -7,6 +7,7 @@ namespace LaraStrict\Tests\Traits;
 use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\ConnectionResolver;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\SQLiteConnection;
 use PDO;
 use PHPUnit\Framework\Assert;
@@ -34,5 +35,18 @@ trait SqlTestEnable
     ): void {
         Assert::assertSame(trim($expectedSql), $query->toSql());
         Assert::assertSame($expectedBindings, $query->getBindings());
+    }
+
+    final protected function assertSql(callable $query, string $sql): void
+    {
+        try {
+            ($query)();
+            Assert::fail('Failed asserting that query was executed.');
+        } catch (QueryException $queryException) {
+            $expected = preg_replace('#\s+#', ' ', $sql);
+            preg_match('#\(SQL: (?<sql>.*)\)$#', $queryException->getMessage(), $matches);
+
+            Assert::assertSame($expected, $matches['sql']);
+        }
     }
 }
